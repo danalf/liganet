216c91d3e13c98253016826a4e2b36a2acbe1f48
alfredo@danalf.de <>
2013-03-29 13:02:38 +0100

    ligaweb entfernen

--- src/Ligaweb/CoreBundle/LigawebCoreBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ligaweb\CoreBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class LigawebCoreBundle extends Bundle
-{
-}
--- src/Ligaweb/UserBundle/UserBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ligaweb\UserBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class UserBundle extends Bundle
-{
-}
