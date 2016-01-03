88dc5ac2643c177601668187a180b87a6a360b81
Alfredo <alfredo@danalf.de>
2016-01-03 09:05:40 +0100

    move spielerstatus

--- src/AppBundle/Controller/SpielerStatusController.php
+++ src/AppBundle/Controller/SpielerStatusController.php
@@ -1,221 +1,142 @@
 <?php
 
-namespace Liganet\CoreBundle\Controller;
+namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Liganet\CoreBundle\Entity\SpielerStatus;
-use Liganet\CoreBundle\Form\SpielerStatusType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use AppBundle\Entity\SpielerStatus;
+use AppBundle\Form\SpielerStatusType;
 
 /**
  * SpielerStatus controller.
  *
  * @Route("/spielerstatus")
+ * @Security("has_role('ROLE_ADMIN')")
  */
 class SpielerStatusController extends Controller
 {
     /**
      * Lists all SpielerStatus entities.
      *
-     * @Route("/", name="spielerstatus")
-     * @Template()
+     * @Route("/", name="spielerstatus_index")
+     * @Method("GET")
      */
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
 
-        $entities = $em->getRepository('LiganetCoreBundle:SpielerStatus')->findAll();
+        $spielerStatuses = $em->getRepository('AppBundle:SpielerStatus')->findAll();
 
-        return array(
-            'entities' => $entities,
-            'isGrantedEdit' => $this->isGrantedEdit()
-        );
-    }
-
-    /**
-     * Finds and displays a SpielerStatus entity.
-     *
-     * @Route("/{id}/show", name="spielerstatus_show")
-     * @Template()
-     */
-    public function showAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('LiganetCoreBundle:SpielerStatus')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find SpielerStatus entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-
-        return array(
-            'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),
-            'isGrantedEdit' => $this->isGrantedEdit()
-        );
-    }
-
-    /**
-     * Displays a form to create a new SpielerStatus entity.
-     *
-     * @Route("/new", name="spielerstatus_new")
-     * @Template()
-     */
-    public function newAction()
-    {
-        if(!$this->isGrantedEdit()){
-            $this->get('session')->getFlashBag()->add('error', 'Neuen Spielerstatus anlegen ist für dich nicht nicht erlaubt');
-            return $this->redirect($this->generateUrl('spielerstatus'));
-        }
-        $entity = new SpielerStatus();
-        $form   = $this->createForm(new SpielerStatusType(), $entity);
-
-        return array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        );
+        return $this->render('spielerstatus/index.html.twig', array(
+            'spielerStatuses' => $spielerStatuses,
+        ));
     }
 
     /**
      * Creates a new SpielerStatus entity.
      *
-     * @Route("/create", name="spielerstatus_create")
-     * @Method("POST")
-     * @Template("LiganetCoreBundle:SpielerStatus:new.html.twig")
+     * @Route("/new", name="spielerstatus_new")
+     * @Method({"GET", "POST"})
      */
-    public function createAction(Request $request)
+    public function newAction(Request $request)
     {
-        $entity  = new SpielerStatus();
-        $form = $this->createForm(new SpielerStatusType(), $entity);
-        $form->bind($request);
+        $spielerStatus = new SpielerStatus();
+        $form = $this->createForm('AppBundle\Form\SpielerStatusType', $spielerStatus);
+        $form->handleRequest($request);
 
-        if ($form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
+            $em->persist($spielerStatus);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('spielerstatus_show', array('id' => $entity->getId())));
+            return $this->redirectToRoute('spielerstatus_show', array('id' => $spielerstatus->getId()));
         }
 
-        return array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        );
+        return $this->render('spielerstatus/new.html.twig', array(
+            'spielerStatus' => $spielerStatus,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a SpielerStatus entity.
+     *
+     * @Route("/{id}", name="spielerstatus_show")
+     * @Method("GET")
+     */
+    public function showAction(SpielerStatus $spielerStatus)
+    {
+        $deleteForm = $this->createDeleteForm($spielerStatus);
+
+        return $this->render('spielerstatus/show.html.twig', array(
+            'spielerStatus' => $spielerStatus,
+            'delete_form' => $deleteForm->createView(),
+        ));
     }
 
     /**
      * Displays a form to edit an existing SpielerStatus entity.
      *
      * @Route("/{id}/edit", name="spielerstatus_edit")
-     * @Template()
+     * @Method({"GET", "POST"})
      */
-    public function editAction($id)
+    public function editAction(Request $request, SpielerStatus $spielerStatus)
     {
-        if(!$this->isGrantedEdit()){
-            $this->get('session')->getFlashBag()->add('error', 'Diesen Spielerstatus zu editieren ist für Dich nicht nicht erlaubt');
-            return $this->redirect($this->generateUrl('spielerstatus_show', array('id' => $id)));
-        }
-        
-        $em = $this->getDoctrine()->getManager();
+        $deleteForm = $this->createDeleteForm($spielerStatus);
+        $editForm = $this->createForm('AppBundle\Form\SpielerStatusType', $spielerStatus);
+        $editForm->handleRequest($request);
 
-        $entity = $em->getRepository('LiganetCoreBundle:SpielerStatus')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find SpielerStatus entity.');
-        }
-
-        $editForm = $this->createForm(new SpielerStatusType(), $entity);
-        $deleteForm = $this->createDeleteForm($id);
-
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-
-    /**
-     * Edits an existing SpielerStatus entity.
-     *
-     * @Route("/{id}/update", name="spielerstatus_update")
-     * @Method("POST")
-     * @Template("LiganetCoreBundle:SpielerStatus:edit.html.twig")
-     */
-    public function updateAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('LiganetCoreBundle:SpielerStatus')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find SpielerStatus entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createForm(new SpielerStatusType(), $entity);
-        $editForm->bind($request);
-
-        if ($editForm->isValid()) {
-            $em->persist($entity);
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($spielerStatus);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('spielerstatus_edit', array('id' => $id)));
+            return $this->redirectToRoute('spielerstatus_edit', array('id' => $spielerStatus->getId()));
         }
 
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
+        return $this->render('spielerstatus/edit.html.twig', array(
+            'spielerStatus' => $spielerStatus,
+            'edit_form' => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
-        );
+        ));
     }
 
     /**
      * Deletes a SpielerStatus entity.
      *
-     * @Route("/{id}/delete", name="spielerstatus_delete")
-     * @Method("POST")
+     * @Route("/{id}", name="spielerstatus_delete")
+     * @Method("DELETE")
      */
-    public function deleteAction(Request $request, $id)
+    public function deleteAction(Request $request, SpielerStatus $spielerStatus)
     {
-        $form = $this->createDeleteForm($id);
-        $form->bind($request);
+        $form = $this->createDeleteForm($spielerStatus);
+        $form->handleRequest($request);
 
-        if ($form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('LiganetCoreBundle:SpielerStatus')->find($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find SpielerStatus entity.');
-            }
-
-            $em->remove($entity);
+            $em->remove($spielerStatus);
             $em->flush();
         }
 
-        return $this->redirect($this->generateUrl('spielerstatus'));
+        return $this->redirectToRoute('spielerstatus_index');
     }
 
-    private function createDeleteForm($id)
+    /**
+     * Creates a form to delete a SpielerStatus entity.
+     *
+     * @param SpielerStatus $spielerStatus The SpielerStatus entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(SpielerStatus $spielerStatus)
     {
-        return $this->createFormBuilder(array('id' => $id))
-            ->add('id', 'hidden')
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('spielerstatus_delete', array('id' => $spielerStatus->getId())))
+            ->setMethod('DELETE')
             ->getForm()
         ;
-    }
-    
-        /**
-     * Legt fest, ob der User (den) Spieltag verändern darf oder nicht
-     * @return boolean
-     */
-    private function isGrantedEdit(){
-        if ($this->get('security.context')->isGranted('ROLE_ADMIN')) {
-            return TRUE;
-        }
-        return FALSE;
     }
 }
--- src/AppBundle/Form/SpielerStatusType.php
+++ src/AppBundle/Form/SpielerStatusType.php
@@ -4,10 +4,14 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SpielerStatusType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -15,12 +19,14 @@ class SpielerStatusType extends AbstractType
             ->add('kurz')
         ;
     }
-
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Liganet\CoreBundle\Entity\SpielerStatus'
+            'data_class' => 'AppBundle\Entity\SpielerStatus'
         ));
     }
-
 }
